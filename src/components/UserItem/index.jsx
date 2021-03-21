762d44d7737815ba2886a4ded477448f83c55876
BodNS <71136538+BodNS@users.noreply.github.com>
2021-03-21 23:49:57 +0200

    Add files via upload

---
+++ src/components/UserItem/index.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import styles from "./index.css";
+import cn from "classnames";
+
+class UserItem extends Component {
+  constructor(props) {
+    super(props);
+    const {
+      user: { id, fName, lName },
+    } = props;
+
+    this.state = {
+      isChecked: false,
+    };
+
+    this.checked = this.checked.bind(this);
+  }
+
+  checked = () => {
+    this.setState((state) => ({
+      isChecked: !isChecked,
+    }));
+
+    const checkedClass = cn(styles.input, { [styles.checked]: isChecked });
+
+    return (
+      <li>
+        <label>
+          <input
+            type="checkbox"
+            onClick={this.checked}
+            className={checkedClass}
+          ></input>
+          UserID: {id} UserFullName: {fName} {lName}
+        </label>
+      </li>
+    );
+  };
+}
+export default UserItem;
---
+++ src/components/UserList/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import UserItem from "../../components/UserItem";
+const UserList = (props) => {
+  const { users, setUsers } = props;
+  const mapUser = (user, index) => {
+    const selectUsersHandler = () => {
+      const newUsers = [...users];
+      newUsers[index].isSelected = !newUsers[index].isSelected;
+      setUsers(newUsers);
+    };
+    return <UserItem key={user.id} user={user} />;
+  };
+  return <ul> {users.map(mapUser)} </ul>;
+};
+
+export default UserList;
