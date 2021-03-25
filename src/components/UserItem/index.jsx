a7ab2926df76c4dc2d4a7bd4d0d9ef81287615e3
BodNS <71136538+BodNS@users.noreply.github.com>
2021-03-25 17:59:38 +0200

    Add files via upload
    popitka 2

--- src/components/UserItem/index.jsx
+++ src/components/UserItem/index.jsx
@@ -1,53 +1,13 @@
 import React, { Component } from "react";
-import styles from "./index.css";
-import cn from "classnames";
 
-class UserItem extends Component {
-  constructor(props) {
-    super(props);
-    const {
-      user: { id, fName, lName },
-    } = props;
-
-    this.state = {
-      isChecked: false,
-    };
-
-    this.checked = this.checked.bind(this);
-  }
-
-  checked = () => {
-    this.setState((state) => ({
-      isChecked: !isChecked,
-    }));
-
-    const checkedClass = cn(styles.input, { [styles.checked]: isChecked });
-
+const UserItem = (props) => {
+    const {user:{id, fName, lName}} = props;
     return (
-      <li>
-        <label>
-          <input
-            type="checkbox"
-            onClick={this.checked}
-            className={checkedClass}
-          ></input>
-          UserID: {id} UserFullName: {fName} {lName}
-        </label>
-      </li>
-    );
-  };
+        <li>
+            <span>UserID: "{id}" UserFullName:"{fName} {lName}"</span>
+        </li>
+    )
+
 }
+
 export default UserItem;
-
-
-
-
-
-
-
-
-
-/* function ListItem(props) {
-  // Правильно! Не нужно определять здесь ключ:
-  return <li>{props.value}</li>;
-} */
--- src/components/UserList/index.jsx
+++ src/components/UserList/index.jsx
@@ -1,37 +1,14 @@
 import React, { Component } from "react";
-import UserItem from "../../components/UserItem";
+import UserItem from "../UserItem";
 
 const UserList = (props) => {
-  const { users } = props;
-  const UserItems = users.map(
-    (user) => 
-    <UserItem key={user.id} user={user} />
-  );
-    
-  return <ul> {UserItems} </ul>;
-};
+    const users = props;
+    const UserItems = users.map((user) => 
+        <UserItem key={user.id} user={user} />
+    )
+    return (
+      <ul> {UserItems}  </ul>
+    )
+}
 
 export default UserList;
-
-
-
-
-
-
-  
-  /* function NumberList(props) {
-    const numbers = props.numbers;
-    const listItems = numbers.map((number) =>
-      // Правильно! Ключ нужно определять внутри массива:
-      <ListItem key={number.toString()} value={number} />
-    );
-    return (
-      <ul>
-        {listItems}
-      </ul>
-    );
-  } */
-  
-  
-    
-    
