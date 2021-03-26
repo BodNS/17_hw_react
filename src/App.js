e57fd321ae5989cf155d03f5d8fe543625a9b810
BodNS <71136538+BodNS@users.noreply.github.com>
2021-03-26 17:48:16 +0200

    Add files via upload
    34

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import "./App.css";
 import React, { Component } from "react";
 import UserList from "./components/UserList";
 
-const users = JSON.parse(users);
+fetch("../src/data/users.json")
+  .then(response => response.json())
+  .then(users => alert(users));
+  
 
 const App = () => {
   <UserList  users={users} />
